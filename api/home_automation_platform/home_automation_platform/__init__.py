00546a0a0e6d350cb29276b30d68f15c00c2ea55
Daniel Sampson <daniel.sampson@gmail.com>
2017-04-17 20:25:00 -0700

    Added an api endpoint to record events.

--- api/home_automation_platform/home_automation_platform/__init__.py
+++ api/home_automation_platform/home_automation_platform/__init__.py
@@ -33,5 +33,15 @@ def log_sensor_record(node_id):
     resp = Response('{ "status":"success" }', status=200, mimetype='application/json')
     return resp
 
+@app.route('/event_records/<int:node_id>', methods=['POST'])
+def log_event_record(node_id):
+    content = request.get_json(force=True)
+
+    # Write sensor readings to database
+    run_db_query("INSERT INTO event_records VALUES (DEFAULT, {0}, '{1}', '{2}');".format(content['event_id'], timestamp, node_id))
+
+    resp = Response('{ "status":"success" }', status=200, mimetype='application/json')
+    return resp
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
